7e7762b31c79351f97acd52fe6317abe94254c9f
LiuCong2017 <2018568647@qq.com>
2022-11-08 06:54:31 +0800

    add new

---
+++ example-other/spring-vue-data-synch/data-synch-spring/data-synch-spring/db/table_test_db.sql
@@ -0,0 +1,22 @@
+use mysql;
+DROP database if EXISTS `table_test`;
+CREATE database `csm_test` default character set utf8 collate utf8_general_ci;
+use `table_test`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `table`;
+CREATE TABLE `table`  (
+                         `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                         `gender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                         `birthday` date NULL DEFAULT NULL,
+                         `salary` double NULL DEFAULT NULL,
+                         `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                         `remark` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL comment 'Remark',
+                         PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+# INSERT INTO `table` VALUES ('Jeromy',1970-12-03,8999.0,'je@je.com','');
+
+SET FOREIGN_KEY_CHECKS = 1;
