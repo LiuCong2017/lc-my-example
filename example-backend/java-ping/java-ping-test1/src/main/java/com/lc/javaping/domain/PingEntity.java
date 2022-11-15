0ee75c44a2858f5fe9b0106154046fa1d049f3fc
LiuCong2017 <2018568647@qq.com>
2022-11-16 07:13:28 +0800

    add new

---
+++ example-backend/java-ping/java-ping-test1/src/main/java/com/lc/javaping/domain/PingEntity.java
@@ -0,0 +1,8 @@
+package com.lc.javaping.domain;
+
+import lombok.Data;
+
+@Data
+public class PingEntity {
+    private String ip;
+}
---
+++ example-backend/java-ping/java-ping-test1/src/test/java/com/lc/javaping/JavaPingTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.javaping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPingTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
