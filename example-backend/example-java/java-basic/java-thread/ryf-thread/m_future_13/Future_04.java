aa0e99db20cddb9dc5131e666c84e7569351cf16
Kavin <2018568647@qq.com>
2022-11-23 08:39:50 +0800

    add new

--- example-backend/example-java/java-basic/java-thread/ryf-thread/m_future_13/Future_04.java
+++ example-backend/example-java/java-basic/java-thread/ryf-thread/m_future_13/Future_04.java
@@ -1,7 +1,41 @@
-/*
- * https://www.liaoxuefeng.com/wiki/1252599548343744/1306581155184674
-* */
 package m_future_13;
 
+import java.util.concurrent.*;
+
 public class Future_04 {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        // 定义任务:
+        Callable<String> task = new Task();
+        // 提交任务并获得Future:
+        Future<String> future = executor.submit(task);
+        // 从Future获取异步执行返回的结果:
+        String result = future.get(); //可能阻塞
+        System.out.println(result);
+        executor.close();
+    }
+
+}
+
+//class Task implements Runnable{
+//    public String result;
+//
+//    @Override
+//    public void run() {
+//        this.result = longTimeCalCulation();
+//    }
+//}
+
+class Task implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        return longTimeCalculation();
+    }
+
+    public String longTimeCalculation() throws InterruptedException {
+        Thread.sleep(2000);
+        return "result";
+    }
 }
